8e70d22351056c77fe37dafedaa87c737b12b0a3
HackerGirl99 <asnaniyas98@gmail.com>
2024-01-02 23:03:46 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { 
+  HomePage, 
+  LoginPage, 
+  SignupPage,
+  ContactUs,
+  Gallery,
+  Leaderboard,
 
-const App = () => {
+} from './routes/Routes';
+
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
