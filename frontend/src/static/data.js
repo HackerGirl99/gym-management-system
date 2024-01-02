3cff9cc5b7e7904921510fe6f7280b1a089b0c87
HackerGirl99 <asnaniyas98@gmail.com>
2024-01-02 23:01:53 +0530

    Updated data link

---
+++ frontend/src/static/data.js
@@ -0,0 +1,24 @@
+// navigation Data
+export const navItems = [
+    {
+      title: "Home",
+      url: "/",
+    },
+    {
+      title: "Log in",
+      url: "/login",
+    },
+    {
+      title: "Contact Us",
+      url: "/contact-us",
+    },
+    {
+      title: "Gallery",
+      url: "/gallery",
+    },
+    {
+      title: "Leaderboard",
+      url: "/leaderboard",
+    },
+  ];
+  
