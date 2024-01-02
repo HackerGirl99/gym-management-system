6b9904f81d524bfda5ec3e4f6256077e0dcabc7f
HackerGirl99 <asnaniyas98@gmail.com>
2024-01-02 22:58:57 +0530

    Added routes

---
+++ frontend/src/routes/Routes.js
@@ -0,0 +1,15 @@
+import LoginPage from "../pages/LoginPage.jsx";
+import SignupPage from "../pages/SignupPage.jsx";
+import HomePage from "../pages/HomePage.jsx";
+import ContactUs from "../pages/ContactUs.jsx";
+import Gallery from "../pages/Gallery.jsx";
+import Leaderboard from "../pages/Leaderboard.jsx";
+export{
+    LoginPage,
+    SignupPage,
+    HomePage,
+    ContactUs,
+    Gallery,
+    Leaderboard
+};
+
