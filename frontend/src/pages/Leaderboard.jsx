da7298598340807f73101eb63d462648057433bd
HackerGirl99 <asnaniyas98@gmail.com>
2024-01-16 11:17:58 +0530

    Added the leaderboard component

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
-
+import LeaderboardComponent from "../components/LeaderBoard/LeaderBoard";
 const Leaderboard = () => {
   return (
     <div>
          <Header activeHeading={5} />
-        Leaderboard
+         <LeaderboardComponent />
           <Footer />
     </div>
   )
