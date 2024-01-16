6096c834ac61b07b2dc1937fa8313ce2561f4f13
HackerGirl99 <asnaniyas98@gmail.com>
2024-01-16 11:13:09 +0530

    Added the registry component to the homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
-
+import Registry from "../components/Registry/Registry";
 const HomePage = () => {
   return (
     <div>
        <Header activeHeading={1} />
-      HomePage
+      <Registry/>
         <Footer />
     
     </div>
