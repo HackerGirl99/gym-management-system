d4e07ccf3115515a9c752cfb26bf6d43d47b7aa9
HackerGirl99 <asnaniyas98@gmail.com>
2024-01-16 11:09:22 +0530

    Admin dashboard page added

---
+++ frontend/src/pages/AdminDashBoardPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AdminDashboardMain from "../components/Admin/AdminDashboardMain";
+
+function AdminDashboardPage() {
+  return (
+    <div>
+        <AdminDashboardMain />
+
+    </div>
+  )
+}
+
+export default AdminDashboardPage
