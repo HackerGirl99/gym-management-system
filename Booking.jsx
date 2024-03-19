f821ed226447099f4b782b6f404ddc5ad7faf3dc
Nadarajah Lucshini <87078374+NadarajahLucshini@users.noreply.github.com>
2024-03-19 17:21:03 +0530

    Add files via upload

---
+++ Booking.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from "../components/Booking/Booking.jsx";
+import Footer from "../components/Layout/Footer";
+import Header from "../components/Layout/Header";
+
+const Booking = () => {
+  return (
+    <div>
+      <Header></Header>
+    <Booking/>
+    <Footer></Footer>
+    </div>
+  )
+}
+
+export default LoginPage
