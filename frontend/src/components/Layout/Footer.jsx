fb62183bddd4afed8e34d7ddda4537e96737a0d6
HackerGirl99 <asnaniyas98@gmail.com>
2024-03-21 06:06:46 +0530

    Common Footer updated

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
 
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className="bg-[#000] text-white">
-     
-      
-
-      <div
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
-         text-center pt-2 text-gray-400 text-sm pb-8"
-      >
+    <div className="bg-[#000] text-white static bottom-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-gray-400 text-sm pb-8">
         <span>© 2023 . All rights reserved.</span>
-        <span>Terms · Privacy Policy</span>
+        <span>
+          <a href="#">Terms</a> · <a href="#">Privacy Policy</a>
+        </span>
         <div className="sm:block flex items-center justify-center w-full">
-          
+          {/* Social media icons as links */}
+          <a href="#" className="mr-4 inline-block">
+            <FaFacebook className="text-xl text-white" />
+          </a>
+          <a href="#" className="mr-4 inline-block">
+            <FaTwitter className="text-xl text-white" />
+          </a>
+          <a href="#" className="inline-block">
+            <FaInstagram className="text-xl text-white" />
+          </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
+
+
+//npm install react-icons - to make the icons work
