1e2e0a2a6640bb3e5ee1afe0c7f154b2df364f79
HackerGirl99 <asnaniyas98@gmail.com>
2024-03-28 12:49:40 +0530

    user model added

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// Define the schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  userType: { type: String, enum: ['STUDENT', 'STAFF'], required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date }
+});
+
+// Create the model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
