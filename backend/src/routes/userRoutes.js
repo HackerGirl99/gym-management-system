b5d1b429881417f6290d6aac621c6b8e6a07cc8d
sharanika <sharanika99@gmail.com>
2024-04-20 19:23:48 +0530

    User_Routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,27 +1,33 @@
 // routes/userRoutes.js
-
-
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 const userController = require('../controllers/userController');
 
+// Define routes
+router.post(
+  '/register',
+  [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    body('userType').isIn(['STUDENT', 'STAFF']).withMessage('User type must be STUDENT or STAFF'),
+  ],
+  userController.createUser
+);
 
-// Apply authMiddleware to routes that need authentication
-router.get('/',userController.getAllUsers);
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').notEmpty().withMessage('Password is required'),
+  ],
+  userController.userLogin
+);
+
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
-
-// Route for user to enter the gym
 router.post('/enter', userController.enterGym);
-
-// Route for user to exit the gym
 router.post('/exit', userController.exitGym);
-
-
-
-// Route for user to exit the gym
-router.post('/exit', userController.exitGym);
-
-
 
 module.exports = router;
