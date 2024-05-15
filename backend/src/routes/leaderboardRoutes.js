b7cbd56fc04c124de05691dcda021ba9f9d46ed4
HackerGirl99 <asnaniyas98@gmail.com>
2024-05-15 15:47:43 +0530

    Redo the backend

--- backend/src/routes/leaderboardRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/leaderboardRoutes.js
-const express = require('express');
-const router = express.Router();
-const leaderboardController = require('../controllers/leaderboardController');
-
-router.get('/', leaderboardController.getLeaderboard); // Removed /leaderboard prefix
-
-module.exports = router;
