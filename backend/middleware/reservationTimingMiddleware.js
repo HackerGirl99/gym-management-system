2a0c8726be81283d0607c0344340090ed58ee6ef
HackerGirl99 <asnaniyas98@gmail.com>
2024-05-16 12:06:44 +0530

    Middleware updated

---
+++ backend/middleware/reservationTimingMiddleware.js
@@ -0,0 +1,8 @@
+// middlewares/reservationTimingMiddleware.js
+async function checkReservationTiming(req, res, next) {
+    // Implemente a lógica de verificação aqui
+    // Se tudo estiver ok, chame next()
+    // Caso contrário, retorne um erro
+}
+
+module.exports = checkReservationTiming;
